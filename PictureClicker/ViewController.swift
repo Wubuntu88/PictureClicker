3c3a4a452d87ba7ce6f7b32664407c13bd2ec8ea
William Edward Gillespie <wgilles1@emich.edu>
2016-03-08 00:45:34 -0500

    The FMDB functionality works correctly.  I was able to create a temporary db and table and insert values as well as perform a count(*) operation which gave me the correct value.

--- PictureClicker/ViewController.swift
+++ PictureClicker/ViewController.swift
@@ -13,7 +13,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let db:FMDatabase = FMDatabase.databaseWithPath("/tmp/tmp.db")
+        
+        let db:FMDatabase = FMDatabase(path: "")
+        let success:Bool = db.open()
+        if success {
+            let createStr:String = "create table bulktest1 (id integer primary key autoincrement, x text);"
+            let insert1 = "insert into bulktest1 (x) values ('XXX');"
+            let insert2 = "insert into bulktest1 (x) values ('YYY');"
+            let insert3 = "insert into bulktest1 (x) values ('ZZZ');"
+            let success2:Bool = db.executeStatements(createStr)
+            db.executeStatements(insert1)
+            db.executeStatements(insert2)
+            db.executeStatements(insert3)
+            if success2 {
+                let select:String = "select count(*) as count from bulktest1;";
+                let resultSet:FMResultSet? = try? db.executeQuery(select, values: [])
+                while(resultSet != nil && resultSet!.next() == true){
+                    if let x:Int32 = resultSet?.intForColumnIndex(0){
+                        print("x: \(x)\n")
+                    }
+                    
+                }
+                
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
