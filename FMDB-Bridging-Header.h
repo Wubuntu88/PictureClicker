16ff84b51e3e5dea823551870258d6bf0a2f5ea4
William Edward Gillespie <wgilles1@emich.edu>
2016-03-07 23:25:56 -0500

    Commiting the initial setup of my project.  At this point, the FMBD files have been imported, and a bridging header has been created (which allows me to use swift when FMDB is written in Obj-C).

---
+++ FMDB-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FMDB-Bridging-Header.h
+//  PictureClicker
+//
+//  Created by William Edward Gillespie on 3/7/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+#ifndef FMDB_Bridging_Header_h
+#define FMDB_Bridging_Header_h
+#import "FMDB.h"
+
+#endif /* FMDB_Bridging_Header_h */
--- PictureClicker/ViewController.swift
+++ PictureClicker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let db:FMDatabase = FMDatabase.databaseWithPath("/tmp/tmp.db")
     }
 
     override func didReceiveMemoryWarning() {
