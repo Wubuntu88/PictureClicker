df9a11798de185a95ef00aee608449fbd88351d0
William Edward Gillespie <wgilles1@emich.edu>
2016-04-10 21:49:04 -0400

    Added some testing to show the pictures to the user.  Now the pictures can be displayed as little images in the tableview.

--- PictureClicker/UserPicturesViewController.swift
+++ PictureClicker/UserPicturesViewController.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+class UserPicturesViewController: UITableViewController {
+    
+    var username:String? = nil
+    let pictureNames:[String] = ["Pictures/probe.jpg", "Pictures/stalker.jpeg",  "Pictures/zealot.jpg", "Pictures/zerglingPikachu.jpg", "Pictures/ultralisk.png", "Pictures/thor.png", "Pictures/roach.png", "Pictures/piplup.png", "Pictures/marine.jpeg", "Pictures/marauder.jpg", "Pictures/jigglypuff.png", "Pictures/hellbat.png", "Pictures/zergling.png", "Pictures/hadoop.png", "Pictures/ghost.jpeg", "Pictures/eevee.png"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return pictureNames.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("genericCell")!
+        if let image:UIImage = UIImage.init(named: pictureNames[indexPath.row]){
+            cell.imageView?.image = image
+        }
+        
+        return cell
+    }
+    
+    
+}
