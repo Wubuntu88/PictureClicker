2d81c5479555760516945613bf82f7c569a09aa4
William Edward Gillespie <wgilles1@emich.edu>
2016-04-10 21:43:54 -0400

    Added pictures and a wrapper for access to my database.

---
+++ PictureClicker/UserPicturesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UserPicturesViewController.swift
+//  PictureClicker
+//
+//  Created by William Edward Gillespie on 4/10/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+import Foundation
