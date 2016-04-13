6fccbdf3a7e631a1e145a7f03366b77f6b409412
William Edward Gillespie <wgilles1@emich.edu>
2016-04-13 18:51:41 -0400

    The StoreViewController is a tableviewcontroller that allows users to see what they can buy.  It is entered though the UserPicturesViewController.  The Make PurchaseViewController shows a large picture of the item and allows the user to purchase that item.

---
+++ PictureClicker/MakePurchaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MakePurchaseViewController.swift
+//  PictureClicker
+//
+//  Created by William Edward Gillespie on 4/13/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+import Foundation
