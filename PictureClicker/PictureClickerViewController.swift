6e37e019d2e3e253a2f8bd8af193b61fb5dc3ce1
William Edward Gillespie <wgilles1@emich.edu>
2016-04-14 16:37:13 -0400

    Setup this view controller to set the IBOutlets to show what is passed to this class via the segue.

--- PictureClicker/PictureClickerViewController.swift
+++ PictureClicker/PictureClickerViewController.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import UIKit
+
+class PictureClickerViewController: UIViewController {
+    var username:String?
+    var db:PicDbWrapper?
+    
+    var item_name:String?
+    var item_location:String?
+    
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemDescription: UILabel!
+    @IBOutlet weak var countdownUntilNextClick: UILabel!
+    
+    @IBAction func tappedOnPicture(sender: UITapGestureRecognizer) {
+        print("clicked \(item_name!)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        itemImage.userInteractionEnabled = true
+        itemImage.image = UIImage(named: item_location!)
+        itemDescription.text = item_name!
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
