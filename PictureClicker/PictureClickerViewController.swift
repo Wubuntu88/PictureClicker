70c6ead36366b6d00b7e9b33a333d2e7e15ec339
William Edward Gillespie <wgilles1@emich.edu>
2016-04-14 16:28:12 -0400

    Added the view controller for when the user want to click on their picture for more points.

---
+++ PictureClicker/PictureClickerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PictureClickerViewController.swift
+//  PictureClicker
+//
+//  Created by William Edward Gillespie on 4/14/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+import Foundation
