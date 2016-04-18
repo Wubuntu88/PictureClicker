6f1d73ebf51f025abdb3e9f186bee92d246c577e
William Edward Gillespie <wgilles1@emich.edu>
2016-04-17 22:32:56 -0400

    Some tidying up

--- PictureClicker/CreateAccountViewController.swift
+++ PictureClicker/CreateAccountViewController.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+class CreateAccountViewController: UIViewController {
+    var db:PicDbWrapper?
+    
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    @IBOutlet weak var notificationLabel: UILabel!
+    
+    @IBAction func createAccount(sender: UIButton) {
+        if usernameLabel.text != "" && passwordLabel.text != "" {
+            let usernameExists = db!.existsUsername(usernameLabel.text!)
+            if usernameExists {
+                notificationLabel.text = "username is taken\ntry again!"
+            }else{
+                db!.addUser(usernameLabel.text!, password: passwordLabel.text!)
+                notificationLabel.enabled = false
+                notificationLabel.text = "Ready to go!\nwelcome \(usernameLabel.text!)"
+                notificationLabel.alpha = 0.5
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
