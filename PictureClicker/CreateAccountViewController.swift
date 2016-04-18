f12171083319f9732e8bede6b894014e738bb911
William Edward Gillespie <wgilles1@emich.edu>
2016-04-17 22:28:50 -0400

    This view controller allows a user to create an account.

---
+++ PictureClicker/CreateAccountViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CreateAccountViewController.swift
+//  PictureClicker
+//
+//  Created by William Edward Gillespie on 4/17/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+import Foundation
